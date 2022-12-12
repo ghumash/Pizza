29fc596b59dd9e88e0ca74e384cd5c8b68bf9c3e
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2022-12-12 20:20:07 +0400

    Add: UI

---
+++ src/components/Categories.jsx
@@ -0,0 +1,14 @@
+export default function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">All</li>
+        <li>Meaty</li>
+        <li>Vegetarian</li>
+        <li>Grill</li>
+        <li>Acute</li>
+        <li>Closed</li>
+      </ul>
+    </div>
+  );
+}
