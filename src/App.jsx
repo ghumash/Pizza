db8eaae6c6d38db7fd7df91248d71ff67e3290ef
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2022-12-16 14:02:43 +0400

    Add: skeleton function for "PizzaBlock" component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
 import PizzaBlockSkeleton from "./components/PizzaBlock/PizzaBlockSkeleton";
-import pizzas from "./assets/pizzas.json";
+import React from "react";
 
 export default function App() {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://637f4b1e2f8f56e28e86f2b5.mockapi.io/pizzas")
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,9 +30,9 @@ export default function App() {
           </div>
           <h2 className="content__title">All pizzas</h2>
           <div className="content__items">
-            {pizzas.map((obj, i) => {
-              return <PizzaBlockSkeleton key={obj.id} {...obj} />;
-            })}
+            {isLoading
+              ? [...new Array(6)].map((_, i) => <PizzaBlockSkeleton key={i} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
--- src/components/PizzaBlock/PizzaBlockSkeleton.jsx
+++ src/components/PizzaBlock/PizzaBlockSkeleton.jsx
@@ -1,20 +1,20 @@
 import ContentLoader from "react-content-loader";
 
-const PizzaBlockSkeleton = (props) => (
+const PizzaBlockSkeleton = () => (
   <ContentLoader
+    className="pizza-block"
     speed={2}
-    width={250}
-    height={500}
+    width={220}
+    height={460}
     viewBox="0 0 280 500"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
-    {...props}
   >
     <circle cx="145" cy="134" r="134" />
-    <rect x="0" y="294" rx="10" ry="10" width="280" height="22" />
-    <rect x="0" y="343" rx="10" ry="10" width="280" height="88" />
-    <rect x="0" y="460" rx="10" ry="10" width="134" height="30" />
-    <rect x="157" y="452" rx="20" ry="20" width="123" height="45" />
+    <rect x="0" y="300" rx="10" ry="10" width="280" height="22" />
+    <rect x="0" y="350" rx="10" ry="10" width="280" height="88" />
+    <rect x="0" y="463" rx="10" ry="10" width="134" height="30" />
+    <rect x="157" y="455" rx="20" ry="20" width="123" height="45" />
   </ContentLoader>
 );
 
